3580c0be62d147d4a4efdc3e08c7d15c26ae42fe
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-01-28 12:31:05 -0500

    using cargo

---
+++ hello_cargo/target/rls/debug/deps/hello_cargo-b2981c52b23728af.d
@@ -0,0 +1,5 @@
+c:\Users\nesto\projects\rust\hello_cargo\target\rls\debug\deps\hello_cargo-b2981c52b23728af.rmeta: src\main.rs
+
+c:\Users\nesto\projects\rust\hello_cargo\target\rls\debug\deps\hello_cargo-b2981c52b23728af.d: src\main.rs
+
+src\main.rs:
