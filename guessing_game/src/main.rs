ba753e43f4442e8c7272c78bb86fc89efcf89a88
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-02-01 12:51:50 -0500

    use external libraries

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,7 +1,10 @@
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Guess the number!");
+    let secret_number = rand::thread_rng().gen_range(0..101);
+    println!("Secret numnber: {}", secret_number);
     println!("Please input your guess!");
     let mut guess = String::new();
     io::stdin()
