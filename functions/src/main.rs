46bffaa522fcdf56ca629d0b26ced9fea9667b37
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-02-04 10:36:43 -0500

    using more complex funcitons

--- functions/src/main.rs
+++ functions/src/main.rs
@@ -2,9 +2,11 @@ fn main() {
     println!("Hello, world!");
     print_something();
     function_with_params(100);
+    let res = function_with_return(100);
+    println!("Result of function_with_params: {}", res);
 }
 
-// Rust use snack case
+// Rust use snack case in functions
 fn print_something() {
     println!("Hi I'm a function");
 }
@@ -12,4 +14,11 @@ fn print_something() {
 fn function_with_params(x: i32) {
     let square: i32 = x * x;
     println!("The square of {} is {}", x, square);
+}
+
+fn function_with_return(x: i32) -> String {
+    let square: i32 = x * x;
+    println!("The square of {} is {}", x, square);
+    let res: String = format!("The square of {} is {}", x, square);
+    return res;
 }
