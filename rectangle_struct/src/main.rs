d96d702dcc2ed2dbd3a00a1e81ec58a287bf208c
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-02-16 12:26:48 -0500

    Improved structs adding impl (functions)

--- rectangle_struct/src/main.rs
+++ rectangle_struct/src/main.rs
@@ -4,18 +4,38 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+    fn can_hold(&self, other: &Rectangle) -> bool {
+        self.width > other.width && self.height > other.height
+    }
+    fn build_square(size: u32) -> Rectangle {
+        Rectangle {
+            height: size,
+            width: size,
+        }
+    }
+}
+
 fn main() {
     println!("Hello, rectangle!");
     let rectangle = Rectangle {
         height: 20,
         width: 100,
     };
-
-    let calc_area = area(&rectangle);
+    let rectangle1 = Rectangle {
+        height: 100,
+        width: 100,
+    };
+    let calc_area = rectangle.area();
     println!("Area: {}", calc_area);
-    println!("Rectangle: {:#?}", rectangle);
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.height * rectangle.width
+    println!("Rectangle 1: {:#?}", rectangle);
+    println!("Rectangle 2: {:#?}", rectangle1);
+    println!(
+        "Rectangle 1 can hold rectangle 2: {:#?}",
+        rectangle.can_hold(&rectangle1)
+    );
+    println!("Square: {:#?}", Rectangle::build_square(100));
 }
