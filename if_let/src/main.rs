403de09fecb223f33f842db1f7189bb76799313a
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2021-03-04 12:22:08 -0500

    Added if let

---
+++ if_let/src/main.rs
@@ -0,0 +1,11 @@
+fn main() {
+    println!("Hello, world!");
+    let some_u8_value = Some(0u8);
+    // match some_u8_value {
+    //     Some(3) => println!("three"),
+    //     _ => (),
+    // }
+    if let Some(3) = some_u8_value {
+        println!("A")
+    }
+}
